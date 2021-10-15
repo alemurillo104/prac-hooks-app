1aec0b55201d0007654dfe0ddcd9ef52670c5f39
Alejandra Murillo <alemurillo104@gmail.com>
2021-10-14 23:33:02 -0400

    fixing problem

--- src/components/02-userEffect/SimpleForm.js
+++ src/components/02-userEffect/SimpleForm.js
@@ -4,16 +4,13 @@ import './effects.css'
 
 
 export const SimpleForm = () => {
-
-
-    //useEffect permite ejecutar algun efecto secundario, cuando algo suceda en nuestros componentes
-
+    
     useEffect( ( ) => {
         fetch("https://random-recipes.p.rapidapi.com/ai-quotes/10", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "random-recipes.p.rapidapi.com",
-                "x-rapidapi-key": "ef0e683726msh8e68d28784092acp195b8djsnc8a075bab5a3"
+                "x-rapidapi-key": "my-api-key"
             }
         })
         .then(response => response.json().then( console.log )
@@ -22,6 +19,12 @@ export const SimpleForm = () => {
             console.error(err);
         });
     })
+
+
+  
+
+    //useEffect permite ejecutar algun efecto secundario, cuando algo suceda en nuestros componentes
+
 
 
     return (
