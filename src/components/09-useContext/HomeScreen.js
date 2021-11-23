4657657876f3b2b5b784c1d3d74319370ee039f3
Alejandra Murillo <alemurillo104@gmail.com>
2021-11-23 15:47:00 -0400

    updating

---
+++ src/components/09-useContext/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// import '../02-userEffect/effects.css'
+
+export const HomeScreen = () => {
+    return (
+        <div>
+            <h1>HomeScreen</h1>
+            <hr />
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { LayoutEffect } from './components/05-useLayoutEffect/LayoutEffect';
 // import { Memorize } from './components/06-memos/Memorize';
@@ -23,7 +24,7 @@ ReactDOM.render(
     // <FormCard />,
     // <ImageToolTip />,
     // <CarrouselCard />,
-    <TodoApp />,
+    <MainApp />,
     document.getElementById('root')
 
 );
