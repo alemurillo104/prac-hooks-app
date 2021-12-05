a36e23fd70c432a223aa96ed7d85b1e751f9184b
Alejandra Murillo <alemurillo104@gmail.com>
2021-12-05 11:20:57 -0400

    updating imports

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
 // import { TodoApp } from './components/08-useReducer/TodoApp';
-import { MainApp } from './components/09-useContext/MainApp';
+// import { MainApp } from './components/09-useContext/MainApp';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { LayoutEffect } from './components/05-useLayoutEffect/LayoutEffect';
 // import { Memorize } from './components/06-memos/Memorize';
 // import { ImageHover } from './components/99-pruebita/ImageHover/ImageHover';
 // import { ImageToolTip } from './components/99-pruebita/ImageHover/ImageToolTip';
 // import { NavCard } from './components/99-pruebita/NavCard/NavCard';
-// import { CarrouselCard } from './components/99-pruebita/CarrouselCard/CarrouselCard';
+import { CarrouselCard } from './components/99-pruebita/CarrouselCard/CarrouselCard';
 
 
 ReactDOM.render(
@@ -23,8 +23,8 @@ ReactDOM.render(
     // />,
     // <FormCard />,
     // <ImageToolTip />,
-    // <CarrouselCard />,
-    <MainApp />,
+    <CarrouselCard />,
+    // <MainApp />,
     document.getElementById('root')
 
 );
